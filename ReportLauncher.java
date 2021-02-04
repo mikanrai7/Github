67eb5ba68af8170d867c9f476a61165f18cf1aeb
mikanrai7 <chamlingrainisha@gmail.com>
2021-02-04 09:33:29 -0600

    To run the task

--- ReportLauncher.java
+++ ReportLauncher.java
@@ -7,3 +7,7 @@ import sailpoint.tools.*;
 
 String reportName = "Oracle Account Disabled Report";
 String fileLocation = "E:\\Tasks\\Disabled Account Report";
+
+//Initiating the task by instantiating a TaskManager and calling RunSync() method for running the task
+TaskManager tm = new TaskManager(context);
+TaskResult result = tm.runSync(reportName,new HashMap());
