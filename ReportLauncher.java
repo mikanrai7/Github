c9bdba6d4aa3391d2dad2059a4b5d2bf627a75dc
mikanrai7 <chamlingrainisha@gmail.com>
2021-02-04 10:09:22 -0600

    queryOptions and Filter

--- ReportLauncher.java
+++ ReportLauncher.java
@@ -11,3 +11,21 @@ String fileLocation = "E:\\Tasks\\Disabled Account Report";
 //Initiating the task by instantiating a TaskManager and calling RunSync() method for running the task
 TaskManager tm = new TaskManager(context);
 TaskResult result = tm.runSync(reportName,new HashMap());
+
+if(null !=result) {
+    JasperResult jr = result.getReport();
+}
+if(null != jr){
+    List fileList = jr.getFiles();
+    for(PersistedFile file: fileList){
+        if(file.isCsv()){
+            String byteString = "";
+            SimpleDateFormat sdf = new SimpleDateFormat("MMddyyyy-HH-mm-ss");
+            Date date = new Date();
+            String fileName = file.getName().substring(0,file.getName().indexOf(".csv")) + "_" + sdf.format(date)+ ".csv";
+            QueryOptions qo = new QueryOptions();
+            Filter ft = Filter.eq("parent",file);
+            qo.addFilter(ft);
+        }
+    }
+}
