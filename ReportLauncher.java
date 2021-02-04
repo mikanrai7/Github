3e1256355e3d86c1c5d1c5f5193910fc52d84cf9
Nisha Rai <chamlingrainisha@gmail.com>
2021-02-04 09:19:10 -0600

    initializing

--- ReportLauncher.java
+++ ReportLauncher.java
@@ -1,1 +1,9 @@
 //This will launch the report and place the file in the specified folder
+
+import sailpoint.object.*;
+import sailpoint.api.*;
+import java.util.*;
+import sailpoint.tools.*;
+
+String reportName = "Oracle Account Disabled Report";
+String fileLocation = "E:\\Tasks\\Disabled Account Report";
